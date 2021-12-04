d1fb0a0157c0155aab2e3bcfeafbdf1467781894
Igor <avt191540@yandex.ru>
2021-12-04 17:10:09 +0300

    calculator is done

--- src/main/java/pro/sky/calculator/controller/FirstController.java
+++ src/main/java/pro/sky/calculator/controller/FirstController.java
@@ -23,5 +23,18 @@ public class FirstController {
     public String plus(@RequestParam int num1, @RequestParam int num2) {
         return (num1 + " + " + num2 + " = " + calculatorService.getPlus(num1, num2));
     }
-
+    @GetMapping("/minus")
+    public String minus(@RequestParam int num1, @RequestParam int num2) {
+        return (num1 + " - " + num2 + " = " + calculatorService.getMinus(num1, num2));
+    }
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam int num1, @RequestParam int num2) {
+        return (num1 + " * " + num2 + " = " + calculatorService.getMultiply(num1, num2));
+    }
+    @GetMapping("/divide")
+    public String divide(@RequestParam int num1, @RequestParam int num2) {
+        if (num2 != 0) {
+            return (num1 + " / " + num2 + " = " + calculatorService.getDivide(num1, num2));
+        } else return "Ошибка: деление на 0 запрещено. Измените значение num2";
+    }
 }
--- src/main/java/pro/sky/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/calculator/service/CalculatorServiceImpl.java
@@ -5,10 +5,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
     private int result;
+    private double result1;
 
     @Override
     public int getPlus(int num1, int num2) {
         result = num1 + num2;
         return result;
+    }
+    @Override
+    public int getMinus(int num1, int num2) {
+        result = num1 - num2;
+        return result;
+    }
+    @Override
+    public int getMultiply(int num1, int num2) {
+        result = num1 * num2;
+        return result;
+    }
+    @Override
+    public double getDivide(int num1, int num2) {
+        result1 = (double) num1 / num2;
+        return result1;
     }
 }
